c44789136cdd6a4c1e2174e94d33478be4d30d79
hf26023 <260239123@qq.com>
2026-03-10 13:17:07 +0800

    first commit

---
+++ docs/source/conf.py
@@ -0,0 +1,73 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'DrestryRobot'
+copyright = '2025, DrestryRobot'
+author = 'DrestryRobot'
+
+# release = '2025.05.04'
+# version = '2025.05.04'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.mathjax',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+
+# -- Options for HTML output
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# -- 添加 Google Analytics 跟踪代码
+html_js_files = [
+    'https://www.googletagmanager.com/gtag/js?id=G-X0VQVVBVYP',
+    'analytics.js',
+]
+
+# -- 自定义网页包含指令
+from docutils import nodes
+from docutils.parsers.rst import Directive
+
+class IncludeHTML(Directive):
+    """
+    自定义指令，用于插入外部的 HTML 文件内容。
+    使用方法：
+    .. includehtml:: path/to/weather.html
+    """
+    required_arguments = 1
+    final_argument_whitespace = True
+
+    def run(self):
+        env = self.state.document.settings.env
+        rel_path, abs_path = env.relfn2path(self.arguments[0])
+        try:
+            with open(abs_path, "r", encoding="utf-8") as f:
+                html_content = f.read()
+        except Exception as err:
+            error = self.state_machine.reporter.error(
+                f"无法包含文件 {abs_path}: {err}",
+                nodes.literal_block(self.block_text, self.block_text),
+                line=self.lineno)
+            return [error]
+        return [nodes.raw("", html_content, format="html")]
+
+def setup(app):
+    app.add_directive("includehtml", IncludeHTML)
---
+++ docs/source/index.rst
@@ -0,0 +1,90 @@
+欢迎来到DrestryRobot😃
+==========================
+.. figure:: _images/蓝白橙色手写体自然景观旅行分享微信公众号封面.gif
+   :alt: Version
+   :align: center
+   :width: 100%
+
+.. raw:: html
+
+   <br>
+
+"发现美，创造美"
+----------------
+这是一个在线网页，旨在于资源分享、技术总结、项目开发和产品展示，欢迎学习与交流！
+
+最近更新
+---------
+- 2025.05.05
+   - DigitalWorld
+   - ModelView
+   - SiteSearch
+      - `SiteSearch <https://drestryrobot.readthedocs.io/zh-cn/latest/_static/SiteSearch/index.html>`_ 官网导航
+      - 优化界面UI设计
+      - 新增和删减部分官网
+- 2025.05.04
+   - SiteSearch
+      - 支持快速搜索各大官网网址
+      - 支持快速分类各大官网网址
+      - 支持快速访问各大官网网址
+
+.. raw:: html
+
+   <details>
+   <summary>点击查看历史更新</summary>
+   <pre>
+   - 2025.05.03
+      - DrestryRobot
+         - DrestryRobot桌面应用程序
+         - 软件界面自动跟随系统深浅色模式进行切换
+         - 软件图标自动跟随系统深浅色模式进行切换
+         - 与网页端保持一致的功能和设计
+         - 支持Windows操作系统
+
+      - 主页面新增实时天气卡片
+         - 打开页面5s后自动收缩为胶囊状态
+         - 点击可在卡片和胶囊状态之间切换
+         - 显示当前时间、位置和天气
+         - 支持夜间模式
+         - 使用IP定位获取位置
+         - 使用WeatherAPI获取天气数据
+         - 温度emoji表情符号根据温度变化
+         - 天气emoji表情符号根据天气变化
+         - 适配手机和电脑屏幕
+
+   - 2025.05.02
+      - 技术总结
+      - 表情符号
+
+   - 2025.05.01
+      - 技术标准
+      - 导纳控制
+      - 负载辨识
+
+   - 2025.04.30
+      - 机械模型
+      - 网页链接
+      - 老资源库
+   </pre>
+   </details>
+
+.. raw:: html
+
+   <br>
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 内容目录
+
+   资源分享
+   技术总结
+   项目开发
+   产品展示
+
+投稿渠道
+----------
+如果你有好的资源、技术、项目或产品，欢迎投稿到邮箱：2371478179@qq.com
+
+.. includehtml:: _static/weather.html
+
+
